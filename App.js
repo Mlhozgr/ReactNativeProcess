1fc8697c237d99c2c6c43cae7be0378ec02c9c38
Melih <mlhozgr@gmail.com>
2019-12-12 21:13:09 +0300

    flex direction işlemleri

--- App.js
+++ App.js
@@ -10,13 +10,12 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Hoşgeldiniz</Text>
-        <View style={styles.arka1}></View>
+        
+        <View style={styles.arka1}>
+          <View style={[styles.box,styles.box2]}></View>
+          <View style={[styles.box,styles.box1]}></View>
+        </View>
         <View style={styles.arka2}></View>
-        
-
-        
-        
       </View>
     );
   }
@@ -26,19 +25,24 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor:'#ddd',
     flex: 1, // arkaplan 1 parça ve container arka1 ve arka2 nin babası
-    
-    
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  
-  arka1: {
-    backgroundColor:'#0C70A5',
-    flex:1              // arka2 nin flexi 2 olduğu için toplam 3 parçanın 1 parçası arka1 in
+    flexDirection:'row'
+    },
 
+ arka1: {
+    backgroundColor:'#0C70A5',
+    flex:1,              // arka2 nin flexi 2 olduğu için toplam 3 parçanın 1 parçası arka1 in
+    flexDirection:'column'
+    },
+  box: {
+    height:100,
+    width:138
+  },
+  box1:{
+    backgroundColor:'#1C2833',
+    
+},
+  box2: {
+    backgroundColor:'#fff'
   },
   arka2 :{
     backgroundColor:'#C73E2E',
