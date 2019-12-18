bcfb4f0bbdcfbaac5e781bb5dc7a6d8e91441f0d
Melih <mlhozgr@gmail.com>
2019-12-18 20:06:49 +0300

    buton islemleri

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
+/* yorum satırı */
 
 import React, {Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View,Button} from 'react-native';
 import Card from './src/components/Card'; 
 
 export default class App extends Component {
+  onPressDetail = () => {                     /* Butonun çağıracağı event */
+    alert('Detail'); 
+  };
+ 
   render() {
     return (
       <View style={styles.container}>
+       
         
-        <View style={styles.arka1}>
-          <View style={[styles.box,styles.box2]}></View>
-          <View style={[styles.box,styles.box1]}></View> 
-        </View>
+         <View style={styles.arka1}> 
+          <View style={[styles.box,styles.box2]}></View>  
+         <View style={[styles.box,styles.box1]}></View>  
+        </View> 
         <View style={styles.arka2}>
           <Text>Merhaba kullanıcı</Text> 
           <Card text='Hello' backgroundColor='#ddd' />                                        
           <Card text='Login' />
           <Card deneme1={2019} text='$' backgroundColor='#fff'/>
+          <Button 
+                title='BUTON'                           /* Buton işlemleri */
+                color='red'
+                onPress={this.onPressDetail}
+                />
         </View>
       </View>
     );
@@ -27,12 +38,12 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor:'#ddd',
     flex: 1, // arkaplan 1 parça ve container arka1 ve arka2 nin babası
-    flexDirection:'row'
+    flexDirection:'row' 
     },
 
  arka1: {
     backgroundColor:'#0C70A5',
-    flex:1,              // arka2 nin flexi 2 olduğu için toplam 3 parçanın 1 parçası arka1 in
+    flex:1,              // arka2 nin flexi 2 olduğu için toplam 3 parçanın 1 parçası arka1 in 
     flexDirection:'column',
     justifyContent:'center' //kutucukları ortaladı
     
@@ -51,8 +62,8 @@ const styles = StyleSheet.create({
   },
   arka2 :{
     backgroundColor:'#C73E2E',
-    flex:2,
-    flexDirection:'column',
+    flex:2,                           
+    flexDirection:'column',                              
     justifyContent:'center',
     alignItems:'center'
     
